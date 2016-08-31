c8b3b16e3e92604ee356ea4c567f62169fd4fdb0
fmhmillet <fernando.hernandez1@upr.edu>
2016-08-31 17:46:11 -0400

    Centralize, Set Size, Borders

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,8 +4,11 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Fernando M. Hern\u00e1ndez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(860, 440);
+		//myFrame.setLocation(860, 440); Lab Desktop
+		myFrame.setLocation(583, 284); // My Laptop
 		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
