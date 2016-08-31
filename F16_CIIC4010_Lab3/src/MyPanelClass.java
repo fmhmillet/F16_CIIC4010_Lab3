13a651769d8afb2686a893eafb1ab0e825271db1
4015a7 <4015a7@zapote.ece.uprm.edu>
2016-08-31 19:26:08 -0400

    Add Star Arrow

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -24,7 +26,7 @@ public class MyPanelClass extends JPanel {
                           //Paint the background
                           g.setColor(Color.LIGHT_GRAY);
                           g.fillRect(x1, y1, width +1, height +1);
-//                        
+                        
 //                        //Draw a border
 //                        g.setColor(Color.red);
 //                        g.drawRect(x1, y1, width, height);
@@ -40,9 +42,34 @@ public class MyPanelClass extends JPanel {
 //                        //Draw a diagonal line 2
 //                        g.setColor(Color.orange);
 //                        g.drawLine(x1, y2, x2, y1);
+//                          
+//                        //Ovall
+//                        g.setColor(Color.darkGray);
+//                        g.fillOval((x2-55)/2,(y2-55)/2, 55, 55);
+//                          
+//                          Polygon p = new Polygon();
+//                          p.addPoint(x1 + 5, y1 + 25);
+//                          p.addPoint(x1 + 20, y1 + 10);
+//                          p.addPoint(x1 + 35, y1 + 25);
+//                          p.addPoint(x1 + 25, y1 + 25);
+//                          p.addPoint(x1 + 25, y1 + 45);
+//                          p.addPoint(x1 + 15, y1 + 45);
+//                          p.addPoint(x1 + 15, y1 + 25);
+//                          g.setColor(Color.YELLOW);
+//                          g.fillPolygon(p);
                           
-                          //Ovall
-                          g.setColor(Color.darkGray);
-                          g.fillOval((x2-55)/2,(y2-55)/2, 55, 55);
+                          Polygon p2 = new Polygon();
+                          p2.addPoint(x1 + 25, y1 + 73);
+                          p2.addPoint(x1 + 41, y1 + 73);
+                          p2.addPoint(x1 + 47, y1 + 58);
+                          p2.addPoint(x1 + 53, y1 + 73);
+                          p2.addPoint(x1 + 69, y1 + 73);
+                          p2.addPoint(x1 + 56, y1 + 83);
+                          p2.addPoint(x1 + 61, y1 + 98);
+                          p2.addPoint(x1 + 47, y1 + 88);
+                          p2.addPoint(x1 + 34, y1 + 98);
+                          p2.addPoint(x1 + 38, y1 + 83);
+                          g.setColor(Color.WHITE);
+                          g.fillPolygon(p2);
             }
 }
